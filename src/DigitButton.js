8d8d6ded47d2b852f0cdfbe811ce9f3f19e31bcc
ahmed abd-elrahman <92221516+ahmed-abdelelrahman@users.noreply.github.com>
2023-02-16 09:03:26 +0200

    Update DigitButton.js
    e.preventDefault() can prevent react from reloading.
    This change will be helpful.

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -3,7 +3,10 @@ import { ACTIONS } from "./App"
 export default function DigitButton({ dispatch, digit }) {
   return (
     <button
-      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+      onClick={(e) =>
+      e.preventDefault();
+      dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } });
+      }
     >
       {digit}
     </button>
